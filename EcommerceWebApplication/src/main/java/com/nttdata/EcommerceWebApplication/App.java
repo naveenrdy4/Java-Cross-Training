7c9bf19ed96be1ddd44632130162a4eb72a8ee9a
naveenrdy4 <naveenr.rock@gmail.com>
2020-10-22 18:58:01 +0530

    Hello Files

---
+++ EcommerceWebApplication/src/main/java/com/nttdata/EcommerceWebApplication/App.java
@@ -0,0 +1,16 @@
+package com.nttdata.EcommerceWebApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a=4;
+    	int b=5;
+    	System.out.println("addition "+(a+b));
+        System.out.println( "Hello World!" );
+    }
+}
